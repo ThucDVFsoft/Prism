7a9e50b2c3e6bfc4a5db0d1259f7e3ff15011a5a
ThucDV <dovanthuc09@gmail.com>
2017-10-26 23:47:18 +0700

    update

---
+++ Command/Command.Module.Order/Models/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Command.Module.Order.Models
+{
+    class OrderRepository : IOrderRepository
+    {
+        private const int InitialOrdersCount = 30;
+
+        public IEnumerable<Order> GetOrdersToEdit()
+        {
+            var orders = new List<Order>(InitialOrdersCount);
+            for (int i = 1; i <= InitialOrdersCount; i++)
+            {
+                string orderName = String.Format(CultureInfo.CurrentCulture, "Order {0}", i);
+                orders.Add(new Order { Name = orderName, DeliveryDate = DateTime.Today.AddDays(i) });
+            }
+
+            return orders;
+        }
+    }
+}
---
+++ Command/Command.Module.Order/ViewModels/OrderViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Command.Module.Order.Models;
+using System.Windows.Data;
+
+namespace Command.Module.Order.ViewModels
+{
+    public class OrderViewModel
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public OrderViewModel()
+        {
+            orderRepository = new OrderRepository();
+            var listOrder = orderRepository.GetOrdersToEdit();
+            Orders = new ListCollectionView(listOrder.ToList());
+        }
+
+        public ICollectionView Orders { get; set; }
+    }
+}
