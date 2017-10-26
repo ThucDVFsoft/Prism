3d4015b9567e4d85294029d99b92120a10803b60
ThucDV <dovanthuc09@gmail.com>
2017-10-26 07:22:52 +0700

    Add New

---
+++ HelloWorld/HelloWorld/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace HelloWorld
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            //ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            ((ModuleCatalog)this.ModuleCatalog).AddModule(typeof(HelloWorldModule.HelloWorldModule));
+        }
+    }
+}
---
+++ HelloWorld/HelloWorldModule/HelloWorldModule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace HelloWorldModule
+{
+    public class HelloWorldModule : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+
+        public HelloWorldModule(IRegionViewRegistry registry)
+        {
+            this.regionViewRegistry = registry;
+        }
+
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(HelloWorldView));
+        }
+    }
+}
