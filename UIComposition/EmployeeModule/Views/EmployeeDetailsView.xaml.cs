fe09a1b1b7d0bb390df2f315aa26dc8df6987e98
ThucDV <thuc.do.van@ntq-solution.com.vn>
2017-11-01 17:47:51 +0700

    Đã hiển thị được màn hình List Employee tuy nhiên chưa có data

---
+++ UIComposition/EmployeeModule/Views/EmployeeDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace EmployeeModule.Views
+{
+    /// <summary>
+    /// Interaction logic for EmployeeDetailsViewModel.xaml
+    /// </summary>
+    public partial class EmployeeDetailsView : UserControl
+    {
+        public EmployeeDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
